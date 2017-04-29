6f67706aa5fc1ce90f35f281c44e2e64a467d311
Ian Jury <ijj@uw.edu>
2017-04-29 14:57:36 -0700

    Added project file

---
+++ MSHackathon/MLKplayer/MLKplayer/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MLKplayer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+        }
+        private void LoadEmbeddedAppFile()
+        {
+            try
+            {
+                Uri pathUri = new Uri("ms-appx:///Videos/video1.mp4");
+                MediaPlayer.Source = MediaSource.CreateFromUri(pathUri);
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    // handle exception.
+                    // For example: Log error or notify user problem with file
+                }
+            }
+        }
+    }
+   
+}
